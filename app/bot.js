c42ec3ee8ff0633e02ae1a3298c7c528c48682d4
r.kalita <r.kalita@apomeds.com>
2024-06-24 17:13:37 +0700

    bot

--- app/bot.js
+++ app/bot.js
@@ -14,6 +14,16 @@ bot.start((ctx) => {
 })
 
 // Listen for the bot being removed from a chat
+bot.command('launch', (ctx) => {
+    const id = ctx.from.id;
+    axios.patch('http://0.0.0.0:3001/api/chat', {id}).then(() => {
+        ctx.replyWithHTML(`Bot has been launched`);
+    }).catch(error => {
+        console.error('Error launching:', error);
+    });
+});
+
+// Listen for the bot being removed from a chat
 bot.on('left_chat_member', (ctx) => {
     if (ctx.leftChatMember.id === ctx.botInfo.id) {
         console.log(`Bot was removed from chat: ${ctx.chat.id}`);
